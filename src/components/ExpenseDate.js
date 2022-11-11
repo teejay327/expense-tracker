551130762ddda99061b4121c1b61fbafd197e52d
teejay327 <mrenergy@hotmail.com>
2022-11-11 23:32:08 +1000

    Added Expenses components

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -5,7 +5,7 @@ function ExpenseDate(props) {
 	const day = props.date.toLocaleString('en-GB', {day: '2-digit'});
 	const year = props.date.getFullYear();
   return(
-    <div>
+    <div className='expense-date'>
 			<div>{ month }</div>
 			<div>{ day }</div>
 			<div>{ year }</div>
