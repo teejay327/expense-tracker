63153e278d0208278975db81ae8ca9e36b28d2a4
teejay327 <mrenergy@hotmail.com>
2023-04-06 10:33:16 +1000

    Added handlers for button in ExpenseForm module

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -18,8 +18,18 @@ const ExpenseForm = () => {
     setEnteredDate(event.target.value);
   }
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+    console.log(expenseData);
+  };
+
   return (
-  <form>
+  <form onSubmit={ submitHandler }>
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
@@ -35,7 +45,7 @@ const ExpenseForm = () => {
       </div>
     </div>
     <div className="new-expense__actions">
-      <button type="submit">Add Expense</button>
+      <button type="submit" >Add Expense</button>
     </div>
   </form>
   );
